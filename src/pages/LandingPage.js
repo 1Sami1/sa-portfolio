0dd022571188864b7e2c7dcf77352306ec77a2a7
Sami Adli <samiadli00@gmail.com>
2022-06-24 20:18:34 -0700

    still working on about section not done yet

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import Particles from '../components/Particles';
 import {FaArrowDown} from 'react-icons/fa'
+import { Link } from 'react-router-dom';
 
 
 function LandingPage() {
+  function scrollBtm() {
+    scrollBtm('a').on('click', function(e) {
+      e.preventDefault();
+      scrollBtm('html, about-sec').animate({ scrollTop: scrollBtm.offset().top}, 500, 'linear');
+    });
+  }
   return (
     <div className='lp-wrapper'>
       <section className='hero-sec'>
@@ -11,11 +18,13 @@ function LandingPage() {
         <h1 className='hero-text'>Creative Developer</h1>
         <div className='bottom-bar'></div>
       </section>
-      <a className='scroll-btn' href="/"><FaArrowDown className='scroll-arrow' /></a>
+      <a className='scroll-btn' href="/" onClick={scrollBtm}><FaArrowDown className='scroll-arrow' /></a>
       <section className='about-sec'>
-        <h1>Next Section</h1>
+        <h1 className='about-sec-h1'>About</h1>
+        <p className='about-sec-p'>I'm Sami! I am a Front-End Developer based in Vancouver, BC.<br/> I take pride in creating [ADJECTIVE] designs! </p>
+        <Link to="/about"><button className='about-cta' to='/about' >More about me</button></Link>
       </section>
-      <Particles />
+      {/* <Particles /> */}
     </div>
   )
 }
