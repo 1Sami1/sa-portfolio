66461cafdc39377ff3beadc6d8892b72e18b2e88
Sami Adli <samiadli00@gmail.com>
2022-06-25 15:33:11 -0700

    Done landing page about section
    Done about section, beginning to work on featured work section on landing page

---
+++ src/components/ProjectCard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function ProjectCard() {
+  return (
+    <div className='card-wrapper'>
+        <img src="" alt="" />
+        <button>See Project</button>
+
+    </div>
+  )
+}
+
+export default ProjectCard
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Particles from '../components/Particles';
 import {FaArrowDown} from 'react-icons/fa'
 import { Link } from 'react-router-dom';
+import ProjectCard from '../components/ProjectCard';
 
 
 function LandingPage() {
@@ -20,9 +21,14 @@ function LandingPage() {
       </section>
       <a className='scroll-btn' href="/" onClick={scrollBtm}><FaArrowDown className='scroll-arrow' /></a>
       <section className='about-sec'>
-        <h1 className='about-sec-h1'>About</h1>
-        <p className='about-sec-p'>I'm Sami! I am a Front-End Developer based in Vancouver, BC.<br/> I take pride in creating [ADJECTIVE] designs! </p>
+        <h2 className='about-sec-h2'>About</h2>
+        <p className='about-sec-p'>I'm Sami! I am a Front-End Developer based in Vancouver, BC.<br/> I take pride in creating excellent designs! </p>
         <Link to="/about"><button className='about-cta' to='/about' >More about me</button></Link>
+      </section>
+      <section className='work-sec'>
+        <h2 className='work-sec-h2'>Featured Work</h2>
+        <ProjectCard />
+
       </section>
       {/* <Particles /> */}
     </div>
