b595ed1587ef75b5edd4be3f737866c8dcf5b7cc
Sami Adli <samiadli00@gmail.com>
2022-06-25 16:15:02 -0700

    added project card content
    Added project card content still have to style it left

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import previewPic from '../images/flimfanSS.png'
 
 function ProjectCard() {
   return (
     <div className='card-wrapper'>
-        <img src="" alt="" />
+        <img src={previewPic} alt="" className='card-preview-pic' />
+        <div className='card-info'>
+            <h3>FilmFanDB</h3>
+            <p>React | Sass | Javascript</p>
+            <p>A movie database website designed 
+               to provide users with the latest movie 
+               information.</p>
+
+        </div>
         <button>See Project</button>
 
     </div>
