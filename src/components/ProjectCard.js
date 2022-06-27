70f60d3a32068432a358c76d1cd94383c1c449b2
Sami Adli <samiadli00@gmail.com>
2022-06-27 14:17:41 -0700

    done styling mobile Proj Card
    Done Styling the project card on mobile, Still have to do desktop styling

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -4,16 +4,18 @@ import previewPic from '../images/flimfanSS.png'
 function ProjectCard() {
   return (
     <div className='card-wrapper'>
+      <div className='card-img'>
         <img src={previewPic} alt="" className='card-preview-pic' />
-        <div className='card-info'>
-            <h3>FilmFanDB</h3>
-            <p>React | Sass | Javascript</p>
-            <p>A movie database website designed 
-               to provide users with the latest movie 
-               information.</p>
-
-        </div>
-        <button>See Project</button>
+      </div>
+      <div className='card-info'>
+          <h3 className='card-proj-title'>FilmFanDB</h3>
+          <p className='card-proj-tags'>React | Sass | Javascript</p>
+          <p className='card-proj-p'>A movie database website designed 
+             to provide users with the latest movie 
+             information.</p>
+          <button className='card-btn'>See Project</button>
+      </div>
+      
 
     </div>
   )
