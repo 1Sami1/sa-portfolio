1429c880e11114f2ceb85508aa56b3515b78d6f7
Sami Adli <96108431+1Sami1@users.noreply.github.com>
2022-07-06 17:37:14 -0700

    Done Contact page

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -9,7 +9,7 @@ function ContactPage() {
     <div className='contact-wrapper'>
       <h1 className='contact-h1'>Contact</h1>
       <p className='contact-p'>Say hi and connect with me on new ideas and projects!!</p>
-      <button className='email-btn'>Let's Chat!</button>
+      <button className='email-btn'><a href='mailto:samiadli00@gmail.com'>Let's Chat!</a></button>
       <div className='icons-wrapper'>
           <a href='https://github.com/1Sami1' target='_blank' ><BsGithub className='social-icon'/></a>
           <a href='https://www.linkedin.com/in/sami-adli-07194a228/' target='_blank'><BsLinkedin className='social-icon' /></a>
