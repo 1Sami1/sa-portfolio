e147eaf84ce9328231e30d6480fe1d969d33e76d
Sami Adli <96108431+1Sami1@users.noreply.github.com>
2022-07-07 18:39:46 -0700

    styled both single project pages

--- src/pages/SingleProject/FilmfanPage.js
+++ src/pages/SingleProject/FilmfanPage.js
@@ -8,6 +8,7 @@ import sortFeatureGif from '../../images/FFsortGIF.gif'
 import muDesktopPic from '../../images/mu-desktop-pic.JPG'
 import muMobilePic from '../../images/mu-mobile-pic.JPG'
 import responsiveGif from '../../images/FFresponsiveGIF.gif'
+import { Link } from 'react-router-dom';
 
 
 function FilmfanPage() {
@@ -34,8 +35,7 @@ function FilmfanPage() {
       </section>
       <section className='overview-section'>
         <h3 className='overview-h3'>Overview</h3>
-        <p className='overview-p'>Where im gonna write the overview for the project and the tech stack going to be under it The favorites feature text will all go here  </p>
-        <p className='overview-p'>Where im gonna write the overview for the project and the tech stack going to be under it The favorites feature text will all go here  </p>
+        <p className='overview-p'>FilmFanDB is a movie database web app that allows users to find information about various movies, sort through different categories on the homepage and add movies to their favorites page! It is built with React.js and it collects real time data through The Movie Databse API.  </p>
         <h4 className='devstack-h4'>Development Stack</h4>
         <ul className='devstack-ul'>
           <li>Javasrcipt/React</li>
@@ -55,10 +55,10 @@ function FilmfanPage() {
         <div>Image 3</div>
       </Slider> */}
       <h4 className='favesfeature-h4'>Favourites Feature</h4>
-      <p className='faves-feature-p'>The favorites feature text will all go here The favorites feature text will all go here The favorites feature text will all go here The favorites feature text will all go here The favorites feature text will all go here </p>
+      <p className='faves-feature-p'>The favorites feature was created for users to save their favorite movies without the need for an account. Local storage is used to save the movies dynamically and display them on the favorites page. Users are also able to unfavorite any movie they no longer want. </p>
       <img className='slider-feature-img' src={favesFeatureGif}></img>
       <h4 className='sortfeature-h4'>Sorting Feature</h4>
-      <p className='sort-p'>The sorting feature text will all go here The sorting feature text will all go here The sorting feature text will all go here The sorting feature text will all go here The sorting feature text will all go here The sorting feature text will all go here The sorting feature text will all go here</p>
+      <p className='sort-p'>This feature allows user to toggle between Top rated, Popular, Upcoming and Now playing. It required the manipulation of the API url in order to target the specific data from the database and display the movies related to the different filters.   </p>
       <img className='slider-feature-img' src={sortFeatureGif}></img>
       </section>
       <section className='design-section'>
@@ -69,11 +69,11 @@ function FilmfanPage() {
         <img className='slider-design-pic' src={responsiveGif}></img>
       </Slider>
       <h4 className='design-mu-h4'>Mock Ups</h4>
-      <p className='design-mu-p'>Mockup text will go here. The sorting feature text will all go here The sorting feature text will all go here The sorting feature text will all go here The sorting feature text will all go here</p>
+      <p className='design-mu-p'>High fidelity mockups were made with Adobe XD using a mobile-first approach. The website design is clean and simple with minimal elements, it focuses on the main content while giving elements plenty of breathing room. The colors are light and give off a fun and playful vibe.</p>
       <h4 className='design-res-h4'>Responsiveness</h4>
-      <p className='design-res-p'>Responsiveness text will go here The sorting feature text will all go here The sorting feature text will all go here The sorting feature text will all go here The sorting feature text will all go here</p>
-      {/* <img></img> */}
+      <p className='design-res-p'>Responsive design is a key aspect in web Development. FilmFanDB was coded and styled mobile-first and works on all screen sizes. With the use of multiple media queries the website has no breakpoints on any screen size between 320px and 1920px</p>
       </section>
+      <Link to="/projects"><button className='projects-cta'>Back to projects</button></Link>
     </div>
   )
 }
--- src/pages/SingleProject/PortfolioPage.js
+++ src/pages/SingleProject/PortfolioPage.js
@@ -3,6 +3,11 @@ import HeroMockupPic from '../../images/Portfolio-mockup-min.png'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick"
+import carouselPic from '../../images/Carouselfeature.png'
+import routerPic from '../../images/routerfeature.png'
+import responsiveGif from '../../images/portResposiveVid.gif'
+import wireframesPic from '../../images/portfolio-wireframes.JPG'
+import { Link } from 'react-router-dom';
 
 
 function PortfolioPage() {
@@ -28,7 +33,7 @@ function PortfolioPage() {
       </section>
       <section className='overview-section'>
         <h3 className='overview-h3'>Overview</h3>
-        <p className='overview-p'>Where im gonna write the overview for the project and the tech stack going to be under it</p>
+        <p className='overview-p'>My personal portfolio, the site you're browsing right now! This site was built with React.js as a platform to showcase my work, design skills and my experience in web development. While it is mainly a static site I utilized React to add some dynamism.</p>
         <h4 className='devstack-h4'>Development Stack</h4>
         <ul className='devstack-ul'>
           <li>Javasrcipt/React</li>
@@ -43,30 +48,29 @@ function PortfolioPage() {
       <section className='features-section'>
         <h3 className='features-h3'>Features</h3>
         <Slider {...settings}>
-        <div>Image 1</div>
-        <div>Image 2</div>
-        <div>Image 3</div>
+        <img className='slider-port-pic' src={carouselPic}></img>
+        <img className='slider-port-pic' src={routerPic}></img>
       </Slider>
-      <h4 className='favesfeature-h4'>carousel</h4>
-      <p>The favorites feature text will all go here </p>
+      <h4 className='favesfeature-h4 carousel'>Carousel</h4>
+      <p className='faves-feature-p'>The Carousel feature was implemented using a lightweight Javasrcipt library called Slick.  </p>
       <img></img>
       <h4 className='sortfeature-h4'>React Router</h4>
-      <p>The sorting feature text will all go here </p>
+      <p className='sort-p'>The sorting feature text will all go here The favorites feature text will all go here The favorites feature text will all go here The favorites feature text will all go here The favorites feature text will all go here The favorites feature text will all go here v</p>
       <img></img>
       </section>
       <section className='design-section'>
         <h3 className='design-h3'>Design</h3>
         <Slider {...settings}>
-        <div>Image 1</div>
-        <div>Image 2</div>
-        <div>Image 3</div>
+        <img className='slider-port-pic' src={wireframesPic}></img>
+        <img className='slider-port-pic' src={responsiveGif}></img>
       </Slider>
-      <h4>Wireframes</h4>
-      <p>Mockup text will go here.</p>
-      <h4>Responsiveness</h4>
-      <p>Responsiveness text will go here</p>
-      <img></img>
+      <h4 className='design-mu-h4'>Wireframes</h4>
+      <p className='design-mu-p'>Mockup text will go here. The favorites feature text will all go here The favorites feature text will all go here The favorites feature text will all go here</p>
+      <h4 className='design-res-h4'>Responsiveness</h4>
+      <p className='design-res-p'>Responsiveness text will go here The favorites feature text will all go here The favorites feature text will all go here The favorites feature text will all go here</p>
       </section>
+      <Link to="/projects"><button className='projects-cta'>Back to projects</button></Link>
+
      
 
 
