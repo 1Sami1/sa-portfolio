0460ae1516e840cac44d67e69309ab1e507f1507
Sami Adli <96108431+1Sami1@users.noreply.github.com>
2022-09-24 12:00:17 -0700

    added 404 page

---
+++ src/pages/Page404.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {FaThumbsDown, } from 'react-icons/fa';
+
+function Page404() {
+  return (
+    <div className='404-page'>
+
+      <p>404 - This page does not exit...  exist </p>
+      <FaThumbsDown/>
+
+    </div>
+  )
+}
+
+export default Page404
