b83712ba3fc5115f0e1d598951802a307168c0c0
Sami Adli <96108431+1Sami1@users.noreply.github.com>
2022-09-23 20:13:38 -0700

    Changed Meta data

--- src/pages/SingleProject/CapstonePage.js
+++ src/pages/SingleProject/CapstonePage.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import HeroMockupPic from '../../images/Dukes-MUv2.svg'
+import { AnimationOnScroll } from 'react-animation-on-scroll'
 
 
 function CapstonePage() {
@@ -13,6 +14,26 @@ function CapstonePage() {
         <a href='https://restaurant.bcitwebdeveloper.ca' target='_blank'><button className='livesite-btn'>Live Site</button></a>
         <a href='https://github.com/1Sami1' target='_blank'><button className='github-btn'>Github</button></a>
         </div>
+
+        <AnimationOnScroll animateIn='animate__fadeInUp' animateOnce='true'>
+      <section className='overview-section'>
+        <h3 className='overview-h3'>Overview</h3>
+        <p className='overview-p'>Duke's Steakhouse</p>
+        <h4 className='devstack-h4'>Development Stack</h4>
+        <ul className='devstack-ul'>
+          <li>WordPress</li>
+          <li>CSS/SASS</li>
+          <li>PHP</li>
+          <li>Adobe XD</li>
+          <li>Adobe Photoshop</li>
+          <li>ACF</li>
+          <li>GitHub</li>
+        </ul>
+      </section>
+      </AnimationOnScroll>
+
+
+
       </section>
 
 
