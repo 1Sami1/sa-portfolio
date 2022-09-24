5688bf0d5360575ecad07ee9cc67efba572ebaf8
Sami Adli <96108431+1Sami1@users.noreply.github.com>
2022-09-24 12:38:35 -0700

    Removed footer from mobile devices

--- src/pages/Page404.js
+++ src/pages/Page404.js
@@ -3,11 +3,11 @@ import {Link} from 'react-router-dom';
 
 function Page404() {
   return (
-    <div className='404-page'>
+    <div className='wrapper-404'>
 
-      <h2>You found the 404 page!</h2>
-      <h3>I couldn't find what you were looking for!</h3>
-      <Link to="/"><button>Back To Home</button></Link>
+      <h2 className='h2-404' >You found the 404 page!</h2>
+      <h3 className='h3-404'>I couldn't find what you were looking for!</h3>
+      <Link to="/"><button className='bth-btn'>Back To Home</button></Link>
       
     </div>
   )
