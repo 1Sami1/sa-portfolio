ccbfad9ffac31d4da0874f70eed97b72c8fedf82
Sami Adli <96108431+1Sami1@users.noreply.github.com>
2022-09-24 12:23:17 -0700

    edited 404 contents

--- src/pages/Page404.js
+++ src/pages/Page404.js
@@ -1,13 +1,14 @@
 import React from 'react'
-import {FaThumbsDown, } from 'react-icons/fa';
+import {Link} from 'react-router-dom';
 
 function Page404() {
   return (
     <div className='404-page'>
 
-      <p>404 - This page does not exit...  exist </p>
-      <FaThumbsDown/>
-
+      <h2>You found the 404 page!</h2>
+      <h3>I couldn't find what you were looking for!</h3>
+      <Link to="/"><button>Back To Home</button></Link>
+      
     </div>
   )
 }
